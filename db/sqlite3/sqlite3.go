628a42ac6d1ca4607995716fc1b39749b84bb300
apple <apple.mangoooo97@gmail.com>
2023-09-20 18:10:36 +0900

    add sqlite3

---
+++ db/sqlite3/sqlite3.go
@@ -0,0 +1,17 @@
+package sqlite3
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB = NewClient()
+
+func NewClient() *sql.DB {
+	db, err := sql.Open("sqlite3", "app.db")
+	if err != nil {
+		panic("Could not open database")
+	}
+	return db
+}
