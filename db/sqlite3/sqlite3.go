dec15f8b6787522ea1558a68a7eabee8d2c28991
apple <apple.mangoooo97@gmail.com>
2023-09-21 22:28:05 +0900

    Update sqlite3.go

--- db/sqlite3/sqlite3.go
+++ db/sqlite3/sqlite3.go
@@ -8,6 +8,28 @@ import (
 
 var DB *sql.DB = NewClient()
 
+func Execute(query string) (any, error) {
+	res, err := DB.Exec(query)
+	return res, err
+}
+
+func CreateSchema() {
+	Execute(`DROP TABLE IF EXISTS user`)
+	Execute(
+		`CREATE TABLE IF NOT EXISTS user (
+			
+		)`,
+	)
+
+	Execute(`DROP TABLE IF EXISTS booru`)
+	Execute(
+		`CREATE TABLE IF NOT EXISTS booru (
+			booru_url TEXT NOT NULL,
+			booru_type INTEGER NOT NULL
+		)`,
+	)
+}
+
 func NewClient() *sql.DB {
 	db, err := sql.Open("sqlite3", "app.db")
 	if err != nil {
