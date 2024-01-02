a2a754fa33aa4adc3e2ea942c456557bf66b20e0
apple <apple.mangoooo97@gmail.com>
2024-01-02 19:16:42 +0900

    add GetImageMock and more

--- utils/image/image.go
+++ utils/image/image.go
@@ -3,31 +3,67 @@ package image
 import (
 	"applemango/boorutan/backend/db/redis"
 	"applemango/boorutan/backend/utils/http"
+	"encoding/json"
 	"fmt"
-
 	"github.com/google/uuid"
 	r "github.com/redis/go-redis/v9"
 )
 
 func DownloadImage(url string) (string, error) {
-	uuid := uuid.NewString()
-	path := fmt.Sprintf("./static/images/%s", uuid)
+	id := uuid.NewString()
+	path := fmt.Sprintf("./static/images/%s", id)
 	err := http.RequestDownloadImage(url, path)
 	if err != nil {
 		return "", err
 	}
-	return uuid, nil
+	_, _ = GetImageMock(id)
+	return id, nil
 }
 
 func GetImage(url string) (string, error) {
-	uuid, err := redis.Get(fmt.Sprintf("image:%s", url))
+	id, err := redis.Get(fmt.Sprintf("image:%s", url))
 	if err != nil && err != r.Nil {
 		return "", err
 	}
 	if err == r.Nil {
-		uuid, err := DownloadImage(url)
-		redis.Push(fmt.Sprintf("image:%s", url), uuid)
-		return uuid, err
+		id, err := DownloadImage(url)
+		redis.Push(fmt.Sprintf("image:%s", url), id)
+		return id, err
 	}
-	return uuid, err
+	return id, err
+}
+
+func GetImageUuid(url string) (string, error) {
+	id, err := redis.Get(fmt.Sprintf("image:%s", url))
+	if err != nil && err != r.Nil {
+		return "", err
+	}
+	if err == r.Nil {
+		return "", err
+	}
+	return id, nil
+}
+
+func GetImageMock(uuid string) ([]Color, error) {
+	cache, err := redis.Get(fmt.Sprintf("image:color:%s", uuid))
+	if err != nil && err != r.Nil {
+		return []Color{}, err
+	}
+	if err == r.Nil {
+		summary, err := GetImageSummary(uuid, 5, 5)
+		if err != nil {
+			return []Color{}, err
+		}
+		summaryJson, err := json.Marshal(summary)
+		if err != nil {
+			return []Color{}, err
+		}
+		_ = redis.Push(fmt.Sprintf("image:color:%s", uuid), string(summaryJson))
+		return summary, nil
+	}
+	var colors *[]Color
+	if err := json.Unmarshal([]byte(cache), &colors); err != nil {
+		return []Color{}, err
+	}
+	return *colors, nil
 }
