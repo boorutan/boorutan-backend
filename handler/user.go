01a1872f6ca1bbbb9180785bc4d92bf3d67b3fa5
apple <apple.mangoooo97@gmail.com>
2024-01-21 18:29:00 +0900

    add login

--- handler/user.go
+++ handler/user.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"applemango/boorutan/backend/user"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Login(c *gin.Context) {
+	var b user.User
+	err := c.Bind(&b)
+	if err != nil || len(b.Id) == 0 {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	c.JSON(http.StatusOK, user.GetUser(b.Id))
+}
